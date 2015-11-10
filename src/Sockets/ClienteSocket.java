48c9876c35b9e97497df9f174252306a40fede73
Wellington <wellington.schu@live.com>
2015-11-10 14:58:27 -0200

    Organização do código e inserção de comentários

--- src/Sockets/ClienteSocket.java
+++ src/Sockets/ClienteSocket.java
@@ -5,36 +5,56 @@ import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class ClienteSocket {
+public class ClienteSocket
+{
 	
-	public static void main (String[] args) throws IOException {
+	public static void main (String[] args) throws IOException
+	{
 		
+		// Utilizado para leitura do teclado
 		Scanner in = new Scanner(System.in);
 		
+		// Variável para armazenar o texto que será digitado
 		String texto = "";
 		
+		// Socket Cliente
 		Socket cliente = null;
 		
+		// Stream (tubo) de saída de dados
 		PrintStream saida = null;
 		
-		try{
-			// Cria o cocket com os parâmetros
+		try
+		{
+			// Cria o socket com os parâmetros
 			cliente = new Socket("127.0.0.1",7000);
 			
 			// Stream de saida de dados
 			saida = new PrintStream(cliente.getOutputStream());
 			
-			do{
+			do
+			{
+				// Lê o teclado do cliente
 				texto = in.nextLine();
 				
+				// Imprime o texto para o stream de saída de dados (Servidor)
 				saida.println(texto);
 				System.out.println("Servidor: " + texto);
-			}while(!"sair".equals(texto));
-		}catch (IOException e){
+			}
+			
+			while(!"sair".equals(texto));
+		}
+		
+		catch (IOException e)
+		{
 			System.out.println("Algo errado aconteceu");
-		}finally{
+		}
+		
+		finally
+		{
+			// Encerra o socket Cliente
 			cliente.close();
 		}
+		
 	}
 
 }
--- src/Sockets/ServidorSocket.java
+++ src/Sockets/ServidorSocket.java
@@ -6,9 +6,12 @@ import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class ServidorSocket {
-	public static void main(String[] args) throws IOException {
-		// Cria o Servidor e seta como nulo;
+public class ServidorSocket
+{
+	public static void main(String[] args) throws IOException
+	{
+		
+		// Cria o Servidor e seta como nulo
 		ServerSocket servidor = null;
 
 		// Cria uma coneção para atender o cliente e seta como nulo
@@ -17,24 +20,53 @@ public class ServidorSocket {
 		// Entrada de Dados
 		BufferedReader entrada = null;
 
-		try {
+		try
+		{
+			
+			/*
+			 * Cria o ServerSocket na porta 7000 se estiver disponível
+			 * Lembrar de não utilizar portas abaixo de 1024,
+			 * estas são utilizadas pelo sistema
+			 */
 			servidor = new ServerSocket(7000);
-
+			
+			/*
+			 * Aguarda uma conexão na porta especificada e retorna
+			 * o socket que irá comunicar com o cliente
+			 */
 			conexao = servidor.accept();
-
+			
+			// Cria um BufferedReader para o canal de stream de entrada de dados do socket conexao
 			entrada = new BufferedReader(new InputStreamReader(conexao.getInputStream()));
-
-			do {
+			
+			// Aguarda por algum dado e imprime a linha recebida quando recebe
+			do
+			{
 				String texto = entrada.readLine();
+				
+				// Se o texto for nulo sai do laço
 				if (texto == null)
 					break;
 				System.out.println("Cliente: " + texto);
-			} while (!"sair".equals(entrada.toString()));
-		} catch (IOException e) {
+			}
+			
+			while (!"sair".equals(entrada.toString()));
+		}
+		
+		catch (IOException e)
+		{
 			System.out.println("Algo Errado Aconteceu");
-		} finally {
+		}
+		
+		finally
+		{
+			// Encerra o socket de comunicação
 			conexao.close();
+			
+			// Encerra o ServerSocket
 			servidor.close();
 		}
+		
 	}
+	
 }
